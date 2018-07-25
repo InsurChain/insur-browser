77e21d8f6475ffd987398b41b87c4fc2716fe20a
崔斌 <cuiminmm@gmail.com>
2018-07-25 11:53:50 +0800

    [REV] update witness location
    [DEL] delete vue dist directory after built

--- src/components/api/location.js
+++ src/components/api/location.js
@@ -15,8 +15,8 @@ export default {
                         organization:'LinkVC',
                         isShow:false,
                         position:{
-                            left:'100px',
-                            top:'120px'
+                            left:'70px',
+                            top:'110px'
                         }
                     }
                     
@@ -79,8 +79,8 @@ export default {
                 { 
                     name:'HongKong',
                     position:{
-                        left:'752px',
-                        top:'250px'
+                        left:'754px',
+                        top:'254px'
                     },
                     details:{
                         type: this.$t('witness.dpos'),
@@ -96,7 +96,7 @@ export default {
                 { 
                     name:'HangZhou',
                     position:{
-                        left:'762px',
+                        left:'760px',
                         top:'230px'
                     },
                     details:{
@@ -106,7 +106,92 @@ export default {
                         isShow:false,
                         position:{
                             left:'560px',
-                            top:'200px'
+                            top:'204px'
+                        }
+                    }
+                },
+                { 
+                    name:'ShenZhe',
+                    position:{
+                        left:'747px',
+                        top:'245px'
+                    },
+                    details:{
+                        type: this.$t('witness.dpos'),
+                        region:this.$i18n.locale == 'zh'?'深圳':'ShenZhen',
+                        organization:this.$i18n.locale == 'zh'?'超级圆桌':'Topmdrt',
+                        isShow:false,
+                        position:{
+                            left:'550px',
+                            top:'210px'
+                        }
+                    }
+                },
+                { 
+                    name:'Sydney',
+                    position:{
+                        left:'875px',
+                        top:'430px'
+                    },
+                    details:{
+                        type: this.$t('witness.dpos'),
+                        region:this.$i18n.locale == 'zh'?'悉尼':'Sydney',
+                        organization:this.$i18n.locale == 'zh'?'风雨同舟':'INSUR-Solidarity',
+                        isShow:false,
+                        position:{
+                            left:'640px',
+                            top:'400px'
+                        }
+                    }
+                },
+                { 
+                    name:'Toronto',
+                    position:{
+                        left:'253px',
+                        top:'203px'
+                    },
+                    details:{
+                        type: this.$t('witness.dpos'),
+                        region:this.$i18n.locale == 'zh'?'多伦多':'Toronto',
+                        organization:this.$i18n.locale == 'zh'?'反重力社区':'Anti-Gravity',
+                        isShow:false,
+                        position:{
+                            left:'274px',
+                            top:'110px'
+                        }
+                    }
+                },
+                { 
+                    name:'ShangHai',
+                    position:{
+                        left:'765px',
+                        top:'228px'
+                    },
+                    details:{
+                        type: this.$t('witness.dpos'),
+                        region:this.$i18n.locale == 'zh'?'上海':'ShangHai',
+                        organization:this.$i18n.locale == 'zh'?'NewTank':'NewTank',
+                        isShow:false,
+                        position:{
+                            left:'564px',
+                            top:'190px'
+                        }
+                    }
+                },
+                { 
+                    name:'Washington',
+                    position:{
+                        left:'255px',
+                        top:'230px'
+                    },
+                    details:{
+                        type: this.$t('witness.dpos'),
+                        region:this.$i18n.locale == 'zh'?'华盛顿':'Washington',
+                        organization:this.$i18n.locale == 'zh'?'INSUR-ZT':'INSUR-ZT',
+                        isShow:false,
+                        position:{
+                            left:'210px',
+                            top:'260px'
                         }
                     }
                 }
